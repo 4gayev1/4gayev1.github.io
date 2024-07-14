e304c747d705850ae724b9c4752b0d8305863604
Vahid <Vahid.Agayev@kapitalbank.az>
2024-07-14 14:55:56 +0400

    Refactoring

--- script.js
+++ script.js
@@ -5,6 +5,53 @@ setTimeout(function(){
 const button = document.querySelector('.submit-button'),
     stateMsg = document.querySelector('.pre-state-msg');
 
+const projectsDiv = document.getElementsByClassName("swiper-wrapper")[0];
+var projectsHTML = "";
+
+const projects= [{
+    "project-title": "La Collection",
+    "used-technologies": "React,Redux,Hooks,API,Bootstrap",
+    "projectUrl": "https://la-collection.netlify.app/",
+    "githubUrl": "https://github.com/4gayev1/La-Collection-ReactJS",
+    "imgSrc": "DemosPreview/La_Collection.png"
+},
+{
+    "project-title": "Spotify Clone",
+    "used-technologies": "React,Hooks,TailwindCSS",
+    "projectUrl": "https://react-spotify-clone41.netlify.app/",
+    "githubUrl": "https://github.com/4gayev1/Spotify-Clone-React",
+    "imgSrc": "DemosPreview/Spotify-Clone_React.png"
+},
+{
+    "project-title": "Movie DB",
+    "used-technologies": "React,Hooks,API,CSS",
+    "projectUrl": "https://react-movietmdb-app.netlify.app/",
+    "githubUrl": "https://github.com/4gayev1/React-moviedb-app",
+    "imgSrc": "DemosPreview/React-movietmdb-app.png"
+},
+{
+    "project-title": "Crypto Currency App",
+    "used-technologies": "React,Axios,API,CSS",
+    "projectUrl": "https://react-currency.netlify.app/",
+    "githubUrl": "https://github.com/4gayev1/Crypto-Currency-app",
+    "imgSrc": "DemosPreview/Cryptocurrencyreview.PNG"
+},
+{
+    "project-title": "SEO & Marketing",
+    "used-technologies": "HTML,CSS,BOOTSTRAP,JS",
+    "projectUrl": "DEMOS/SEO&Marketing/SEO&Marketing.html",
+    "githubUrl": "Downloadable/SEO&Marketing.zip",
+    "imgSrc": "DemosPreview/SEO&Marketing.png"
+},
+{
+    "project-title": "Food & Restaurants",
+    "used-technologies": "HTML,CSS,JS,JQuery",
+    "projectUrl": "DEMOS/Food&Restaurant/index.html",
+    "githubUrl": "Downloadable/Food&Restaurant.zip",
+    "imgSrc": "DemosPreview/Food&Restaurant.png"
+}
+]
+
 const updateButtonMsg = function () {
     button.classList.add('state-1', 'animated');
 
@@ -17,6 +64,23 @@ const finalButtonMsg = function () {
 button.addEventListener('click', updateButtonMsg);
 
 
+projects.map(project=>{
+projectsHTML+=`
+<div class="slider-item-container item">
+  <div class="slider-item">
+    <div class="item-detail">
+      <p class="project-title">${project["project-title"]}</p>
+      <p class="used">Used - ${project["used-technologies"]}</p>
+      <a href="${project["projectUrl"]}" target="__blank">Demo</a><br>
+      <a href="${project["githubUrl"]}">Download</a>
+    </div>
+    <img class="slider-img" src="${project["imgSrc"]}" alt="">
+  </div>
+</div>
+`});
+
+projectsDiv.innerHTML=projectsHTML
+
 $('input[name=checkbox]').change(function () {
     if ($(this).is(':checked')) {
         $("body").css("background-color","#131417");
@@ -27,10 +91,7 @@ $('input[name=checkbox]').change(function () {
         $(".text-pro").css("color","#f8f9fa");
         $("hr").css("background-color","white");
         $(".nav-log").attr("src", "logo1-white.png");
-        $(".social-icon-git").attr("src","/icons/icons8-github-50-white.png");
-        $(".social-icon-link").attr("src","/icons/icons8-linkedin-50-white.png");
-        $(".social-icon-fb").attr("src","/icons/icons8-facebook-50-white.png");
-        $(".social-icon-ins").attr("src","/icons/icons8-instagram-50-white.png");
+        $(".icons").css("fill","#FFFFFF");
         $(".slider-items").css("border","1px solid #fff");
     } else {
         $("body").css("background-color","#ffffff");
@@ -42,10 +103,7 @@ $('input[name=checkbox]').change(function () {
         $("hr").css("background-color","black");
         $(".nav-log").attr("src","logo1.png");
         $(".footer-div").css("background-color", "#6c757d");
-        $(".social-icon-git").attr("src","/icons/icons8-github-50.png");
-        $(".social-icon-link").attr("src","/icons/icons8-linkedin-50.png");
-        $(".social-icon-fb").attr("src","/icons/icons8-facebook-50.png");
-        $(".social-icon-ins").attr("src","/icons/icons8-instagram-50.png");
+        $(".icons").css("fill","#000000");
         $(".slider-items").css("border","1px solid #39404f");
     }
 });
@@ -57,6 +115,7 @@ $('.swiper-wrapper').owlCarousel({
     autoplayTimeout:2000,
     autoplayHoverPause:true,
     nav:false,
+    dots:false,
     margin:10,
     responsiveClass:true,
     responsive:{
@@ -76,4 +135,6 @@ $('.swiper-wrapper').owlCarousel({
         }
     }
   });
+
+  
 
